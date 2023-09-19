83b4bb6a7ecaff33321d6010f119b88de37e264d
Harshil <harshilbhatia02@gmail.com>
2023-09-19 19:44:34 +0530

    Updates made

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,32 @@
-window.onload = function() {
-    document.querySelector('.profileImage').style.opacity = 1;
+window.onload = function () {
+  document.querySelector('.profileImage').style.opacity = 1;
 };
 
-const toggle = document.querySelector('#toggleDarkMode');
-const body = document.querySelector('body');
+document.addEventListener('DOMContentLoaded', function () {
+  const darkModeButton = document.getElementById('toggleDarkMode');
+  const body = document.body;
 
-toggle.addEventListener('click', function () {
-  body.classList.toggle('darkMode');
+  darkModeButton.addEventListener('click', function () {
+    body.classList.toggle('darkMode');
+  });
+});
+
+const navigationLinks = document.querySelectorAll('.navigation-menu a');
+
+navigationLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const targetId = link.getAttribute('href').substring(1);
+    const targetSection = document.getElementById(targetId);
+
+    if (targetSection) {
+      window.scrollTo({
+        top: targetSection.offsetTop - document.querySelector('.top-bar').offsetHeight,
+        behavior: 'smooth',
+      });
+    }
+  });
 });
 
 const projects = document.querySelectorAll('.project');
